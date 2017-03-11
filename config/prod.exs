0b42db555b9349f61e6012bc02f26154aafa7392
yianna <yianna@thoughtbot.com>
2017-03-11 13:56:05 -0500

    Change :hello_phoenix in config to :learnathon

--- config/prod.exs
+++ config/prod.exs
@@ -9,7 +9,7 @@ config :learnathon, Learnathon.Web.Endpoint,
 
 config :logger, level: :info
 
-config :hello_phoenix, Learnathon.Repo,
+config :learnathon, Learnathon.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
