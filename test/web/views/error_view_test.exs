661768e4b8f28eb4174614597787e747375b5da5
yianna <yianna@thoughtbot.com>
2017-03-14 21:04:46 -0400

    Add tags 'yup' and 'no' to error_view_test to play with tags

--- test/web/views/error_view_test.exs
+++ test/web/views/error_view_test.exs
@@ -1,14 +1,18 @@
 defmodule Learnathon.Web.ErrorViewTest do
   use Learnathon.Web.ConnCase, async: true
 
+  @moduletag :error_view_case
+
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
+  @tag individual_test: "yup"
   test "renders 404.html" do
     assert render_to_string(Learnathon.Web.ErrorView, "404.html", []) ==
            "Page not found"
   end
 
+  @tag individual_test: "no"
   test "render 500.html" do
     assert render_to_string(Learnathon.Web.ErrorView, "500.html", []) ==
            "Internal server error"
